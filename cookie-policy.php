50c5f74007a4ac47777d6106cb14f1ef90564b7d
Eros <erosdg@libero.it>
2020-04-24 11:27:27 +0200

    Create cookie-policy.php

---
+++ cookie-policy.php
@@ -0,0 +1,82 @@
+Cookies Policy.
+
+0. COSA SONO I COOKIE
+Il sito www.sitocliente.it utilizza i cookie per rendere i propri servizi semplici ed efficienti per
+l’utenza che ne visiona le pagine. I cookie sono piccoli file di testo che i siti visitati dagli utenti
+inviano ai loro terminali, dove vengono memorizzati per essere ritrasmessi agli stessi siti in
+occasione di visite successive. I cookie sono utilizzati per diverse finalità, hanno caratteristiche
+diverse, e possono essere utilizzati sia dal titolare del sito che si sta visitando, sia da terze parti.
+Esistono vari tipi di cookie, a seconda delle loro caratteristiche e funzioni.
+
+1. COOKIE TECNICI
+Questa tipologia di cookie permette il corretto funzionamento di alcune sezioni del sito. I cookie
+tecnici sono di due categorie:
+• persistenti: una volta chiuso il browser non vengono distrutti ma rimangono fino ad una data di
+scadenza preimpostata;
+• di sessione: vengono distrutti ogni volta che il browser viene chiuso.
+
+Questi cookie sono necessari per visualizzare correttamente il sito e in relazione ai servizi tecnici
+offerti, verranno quindi sempre utilizzati e inviati, a meno che l’utenza non modifichi le
+impostazioni nel proprio browser. Tali cookie non richiedono un consenso esplicito per il loro
+utilizzo. Sono considerati cookie tecnici anche:
+• i “cookie analytics” nel caso vengano utilizzati direttamente dal gestore del sito per raccogliere
+informazioni in forma aggregata, sul numero di utenti e su come questi visitano il sito;
+• i cookie di navigazione o di sessione (necessari per l’autenticazione);
+• i cookie di funzionalità che migliorano l’esperienza di navigazione dell’utente.
+
+2. COOKIE DI PROFILAZIONE
+Questa tipologia di cookie è volta a creare profili relativi all'utente e utilizzati al fine di inviare
+messaggi pubblicitari in linea con le preferenze manifestate dallo stesso nell'ambito della
+navigazione in rete. In questo caso, è richiesto un preventivo consenso dell'utente.
+
+3. COOKIE UTILIZZATI DAL SITO
+Questo sito utilizza sia cookie tecnici che di profilazione di terze parti. Qui l’elenco dei cookie di
+terze parti con i relativi link alle informazioni:
+a)Google: https://www.google.com/policies/privacy/partners/ utilizza cookies analytics e di
+profilazione
+Google analytics: http://www.google.it/intl/it/analytics: utilizza cookie analytics al fine di valutare
+la performance del sito e il traffico effettuato su di esso.
+Link a privacy policy: https://support.google.com/analytics/answer/6004245;
+Link pagina di descrizione cookie installati: https://developers.google.com/analytics/devguides/
+collection/analyticsjs/cookie-usage;
+Link a pagina di download allo strumento che permette la disabilitazione della installazione di tali
+cookie: https://tools.google.com/dlpage/gaoptout;
+Cookie di profilazione che potrebbero essere utilizzati: _ga, _gid, _gat, _utma, _utmt, _utmb,
+_utmc, _utmz, _utmv, gac, AMP_TOKEN.
+
+4. DE-SELEZIONE COOKIE DI TERZE PARTI
+ATTENZIONE: disabilitando i cookie tecnici e/o di funzionalità il sito potrebbe risultare non
+consultabile o alcuni servizi o determinate funzioni del sito potrebbero risultare non disponibili o
+non funzionare correttamente e potresti essere costretto a modificare o a inserire manualmente
+alcune informazioni o preferenze ogni volta che visiterà il sito. I cookie di terze parti, cioè cookie di
+siti o di web server diversi da quello di RAGIONE SOCIALE CLIENTE, sono utilizzati per
+finalità proprie di dette parti terze. Si precisa che tali soggetti terzi, di seguito elencati con i relativi
+collegamenti alle politiche sulla privacy, sono tipicamente autonomi titolari del trattamento dei dati
+raccolti attraverso i cookie da essi serviti; pertanto, dovrà farsi riferimento alle loro politiche di
+trattamento di dati personali, informative e moduli di consenso. Per quanto concerne le terze parti
+che inviano cookie tramite il sito, forniamo di seguito i link alle rispettive informative privacy: a tali
+terze parti demandiamo, come già precisato, la responsabilità di fornire l'informativa e raccogliere il
+suo consenso. Detta responsabilità è da riferirsi non solo ai cookie che le terze parti inviano
+direttamente, ma anche ad eventuali ulteriori cookie che vengono inviati tramite il sito in virtù
+dell'utilizzo di servizi di cui le terze parti stesse fruiscono. Rispetto a tali cookie infatti, inviati da
+fornitori di servizi delle citate terze parti, RAGIONE SOCIALE CLIENTE non ha la possibilità
+di esercitare alcun controllo e non ne conosce né le caratteristiche né le finalità.
+
+Cliccando sul bottone Rifiuta che compare nel PopUp relativo alla gestione dei coockies del sito, il
+sistema provvede a settare in anonimo la gestione dei cookies relativi a Google Analytics. Tale
+opzione non è disponibile per il servizio di chat con il quale l'utente spontaneamente può scegliere
+se utilizzare o meno.
+
+Può bloccare o cancellare (in tutto o in parte) i cookie tecnici e di funzionalità anche attraverso le
+specifiche funzioni del suo Browser. Per avere maggiori informazioni su come impostare le
+preferenze sull'uso dei cookie attraverso il Browser, è possibile consultare le relative istruzioni:
+Internet Explorer
+Firefox
+Chrome
+Safari
+Safari (dispositivi mobili)
+
+RAGIONE SOCIALE CLIENTE
+Indirizzo
+P.IVA XXXXXXXX
+R.E.A. TV - XXXX
