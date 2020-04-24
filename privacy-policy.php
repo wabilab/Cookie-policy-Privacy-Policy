306c2309ee3b0fceb0336c873465e78d52e9df9e
Eros <erosdg@libero.it>
2020-04-24 11:51:41 +0200

    Create privacy-policy.php

---
+++ privacy-policy.php
@@ -0,0 +1,183 @@
+Informativa.
+
+Informativa resa ai sensi degli artt. 13-14 del GDPR (General Data Protection Regulation) n.
+2016/679 (di seguito "Regolamento")
+
+0. PREMESSA
+Con la presente pagina si intende informare l'utenza in merito alle politiche osservate da
+RAGIONE SOCIALE CLIENTE, con sede in indirizzo, in materia di raccolta, conservazione,
+utilizzo ed eventuale divulgazione di ogni informazione personale degli utenti assunta nell'ambito
+della fruizione dei servizi offerti nel sito www.sitoweb.it. La presente privacy policy è soggetta ad
+aggiornamenti e modifiche. Tali eventualità saranno comunicate via newsletter o saranno messe in
+evidenza nelle apposite sezioni del sito. Si consiglia di controllare in ogni caso la presente privacy
+policy con periodicità. RAGIONE SOCIALE CLIENTE garantisce il rispetto delle normative
+sopra richiamate, invitando gli utenti a leggere con attenzione la presente Privacy Policy prima di
+inviare qualsiasi informazione personale, specificando che la presente informativa è resa solamente
+per il presente sito. La gestione e la proprietà del sito www.sitoweb.it appartengono esclusivamente
+a RAGIONE SOCIALE AZIENDA.
+
+1. TITOLARE DEL TRATTAMENTO DEI DATI E RESPONSABILE DELLA
+PROTEZIONE DEI DATI
+Il titolare del trattamento dei dati è RAGIONE SOCIALE AZIENDA, P.IVA XXXXXXXXX, con
+sede legale in indirizzo - Italia. È possibile contattare l’azienda all’indirizzo: MAIL DEL
+CLIENTE per qualunque informazione inerente il trattamento dei dati personali di RAGIONE
+SOCIALE CLIENTE, tra cui l’elenco dei responsabili che trattano dati
+
+2. TIPOLOGIE DI DATI RACCOLTI
+Il Titolare del Trattamento, informa che, per il normale funzionamento del sito, tratterà dati che per
+loro natura possono essere definiti come dati personali. Per dato personale si intende qualunque
+informazione relativa a persona fisica, identificata o identificabile, anche indirettamente, mediante
+riferimento a qualsiasi altra informazione, ivi compreso un numero di identificazione personale.
+
+I dati personali trattati attraverso il sito sono i seguenti:
+a) Dati di Navigazione
+Durante il normale funzionamento del sito, i sistemi informatici e le procedure software
+acquisiscono alcuni dati personali la cui trasmissione è implicita nell'uso dei protocolli di
+comunicazione di Internet. Si tratta di informazioni che non sono raccolte per essere associate a
+interessati identificati, ma che per loro stessa natura potrebbero, attraverso elaborazioni ed
+associazioni con dati detenuti da terzi, permettere di identificare gli utenti. In questa categoria di
+dati rientrano gli indirizzi IP o i nomi a dominio dei computer utilizzati dagli utenti che si
+connettono al sito, gli indirizzi in notazione URI (Uniform Resource Identifier) delle risorse
+richieste, l'orario della richiesta, il metodo utilizzato nel sottoporre la richiesta al server, la
+dimensione del file ottenuto in risposta, il codice numerico indicante lo stato della risposta data dal
+server (buon fine, errore, ecc.) ed altri parametri relativi al sistema operativo e all'ambiente
+informatico dell'utente.
+
+b) Dati personali comuni
+Per accedere a determinati servizi del sito, potrebbe essere richiesto il conferimento di dati personali
+comuni. Tali dati personali potrebbero essere, a titolo esemplificativo e non esaustivo: nome,
+cognome, indirizzo e-mail, numero di telefono, ecc.
+
+c) Speciali categorie di dati personali
+Nell’utilizzo del form della sezione contatti per l’invio di candidature, potrebbe verificarsi un
+conferimento di dati personali rientranti nelle categorie citate dall’art. 9 del Regolamento, i
+cosiddetti dati particolari: “[…] dati idonei a rivelare l'origine razziale o etnica, le opinioni
+politiche, le convinzioni religiose o filosofiche, o l'appartenenza sindacale, nonché […] dati
+genetici, dati biometrici intesi a identificare in modo univoco una persona fisica, dati relativi alla
+salute o alla vita sessuale o all'orientamento sessuale della persona”. Tali categorie di dati verranno
+trattati previo consenso da parte dell’interessato.
+Informiamo inoltre che, ai fini della raccolta candidature, RAGIONE SOCIALE CLIENTE
+potrebbe analizzare i suoi profili social a carattere professionale, da lei resi liberamente disponibili
+su Internet (es. LinkedIn).
+
+d) Cookies
+Si veda la cookie policy al seguente link.
+
+3. FINALITÀ DEL TRATTAMENTO
+I dati da voi forniti, compresi quelli di natura personale ed eventualmente particolare, saranno
+oggetto di trattamento per le seguenti finalità:
+a. rispondere ad eventuali richieste di contatto pervenute mediante e-mail o compilando l’apposito
+form nella pagina Contatti;
+b. instaurare un canale immediato domanda/risposta attraverso una chat in tempo reale
+raggiungibile da ogni pagina del sito;
+c. pubblicare commenti riferiti agli articoli del blog;
+d. raccogliere candidature, valutare i CV e ricontattare eventualmente i candidati;
+e. inviare newsletter o comunicazioni di tipo commerciale e di marketing;
+f. espletare attività amministrativo-contabili in genere. Ai fini dell'applicazione delle disposizioni in
+materia di protezione dei dati personali, i trattamenti effettuati per finalità amministrativo-contabili
+sono quelli connessi allo svolgimento delle attività di natura organizzativa, amministrativa,
+finanziaria e contabile, a prescindere dalla natura dei dati trattati. In particolare, perseguono tali
+finalità le attività organizzative interne, quelle funzionali all'adempimento di obblighi contrattuali e
+precontrattuali e l’attività informativa.
+
+4. MODALITÀ DEL TRATTAMENTO
+Il trattamento sarà svolto in forma cartacea e/o informatizzata, ma in ogni caso con misure idonee a
+garantire la massima sicurezza, ad opera di soggetti appositamente formati e incaricati. I dati
+raccolti per le finalità di cui alla sez. 3(a,b,c,f) verranno conservati per il tempo strettamente
+necessario all’assolvimento delle finalità stesse. Per quanto riguarda la conservazione dei dati in
+funzione della finalità di raccolta candidature di cui alla sezione 3(d), il tempo individuato è di mesi
+12 dalla data di ricezione della candidatura. Nel caso invece del trattamento di dati ai fini
+commerciali e di marketing di cui alla sezione 3(e), il tempo di conservazione è stato fissato in mesi
+24. Maggiori informazioni in merito al periodo di conservazione dei dati e ai criteri utilizzati per
+determinare tale periodo possono essere richieste scrivendo a RAGIONE SOCIALE CLIENTE al
+seguente indirizzo: MAIL DEL CLIENTE
+
+5. BASE LEGALE E NATURA OBBLIGATORIA O FACOLTATIVA DEL TRATTAMENTO
+La base legale del trattamento di dati personali per la finalità di cui alla sezione 3 (d,e) è l’art. 6(1)
+(a) del Regolamento in quanto i trattamenti sono subordinati all’acquisizione di esplicito consenso.
+La base legale del trattamento di dati personali per le finalità di cui alla sezione 3 (a,b,c,f) è l’art.
+6(1)(b) del Regolamento in quanto i trattamenti sono necessari all'erogazione dei servizi o per il
+riscontro di richieste dell’interessato. Il conferimento dei dati personali per queste finalità è
+facoltativo ma l'eventuale mancato conferimento comporterebbe l'impossibilità di attivare i servizi
+forniti dal sito, rispondere alle richieste richieste o valutare i CV. Con specifico riferimento alla
+finalità di cui alla sezione 3(d) e alla connessa analisi dei profili social a carattere professionale resi
+liberamente disponibili su Internet di cui alla sezione 2, la base legale del trattamento è l’art. 6(1)(f)
+del Regolamento, ossia il legittimo interesse di RAGIONE SOCIALE CLIENTE a verificare
+eventuali rischi sull’idoneità del candidato a ricoprire specifiche posizioni.
+
+6. DESTINATARI DEI DATI PERSONALI
+I suoi dati personali potranno essere condivisi, per le finalità di cui alla sezione 3, con:
+a. soggetti che agiscono in qualità di responsabili del trattamento ossia: soggetti con i quali sia
+necessario interagire per l'erogazione dei servizi (ad esempio gli hosting provider), oppure soggetti
+delegati a svolgere attività di manutenzione tecnica;
+b. soggetti, enti od autorità a cui sia obbligatorio comunicare i suoi dati personali in forza di
+disposizioni di legge o di ordini delle autorità;
+c. persone autorizzate da RAGIONE SOCIALE CLIENTE al trattamento di dati personali
+necessario a svolgere attività strettamente correlate all'erogazione dei servizi, che si siano
+impegnate alla riservatezza (es. dipendenti di RAGIONE SOCIALE CLIENTE).
+
+7. TRASFERIMENTI DEI DATI PERSONALI
+Per rendere possibile l’esecuzione delle finalità di cui alla sezione 3(e), i dati raccolti potranno
+essere trasferiti all’estero, anche al di fuori dell’Unione Europea nelle forme e nei modi previsti alla
+vigente normativa garantendo in ogni caso un adeguato livello di protezione. Per l’invio di
+comunicazioni a carattere commerciale e di marketing infatti, RAGIONE SOCIALE CLIENTE si
+avvale di Mailchimp o altri strumenti similari, Titolare Autonomo del Trattamento, una piattaforma
+di mailing proprietaria, che attraverso sistemi di tracciamento statistico consente di rilevare
+l’apertura di un messaggio, i click effettuati sui collegamenti ipertestuali contenuti all’interno
+dell’email, da quale indirizzo IP o con quale tipo di browser viene aperta l’email, e altri dettagli
+simili. La raccolta di tali dati è funzionale all’utilizzo della piattaforma e costituisce parte integrante
+delle funzionalità del sistema di invio dei messaggi.
+
+8. DIRITTO DEGLI INTERESSATI
+All’interessato sono riconosciuti specifici diritti:
+1. l’interessato ha diritto di ottenere la conferma dell’esistenza o meno di dati personali che lo
+riguardano, anche se non ancora registrati, e la loro comunicazione in forma intelligibile;
+2. l’interessato ha inoltre diritto di ottenere l’indicazione: dell’origine dei dati personali; delle
+finalità e modalità di trattamento; della logica applicata in caso di trattamento effettuato con
+l’ausilio di strumenti elettronici; degli estremi identificativi del titolare, dei responsabili e del
+rappresentante designato e dei relativi dati di contatto; dei soggetti o delle categorie di soggetti ai
+quali i dati personali possono essere comunicati o che possono venirne a conoscenza in qualità di
+rappresentante designato nel territorio dello Stato, di responsabili o incaricati.
+3. l’interessato ha altresì diritto di ottenere: i legittimi interessi perseguiti dal titolare del trattamento
+o da terzi, gli eventuali destinatari o le eventuali categorie dei destinatari dei dati personali;
+l’intenzione del titolare del trattamento di trasferire i dati personali a un paese terzo o a
+un’organizzazione internazionale e l’esistenza o l’assenza di una decisione di adeguatezza della
+Commissione; il periodo di conservazione dei dati o, se non è possibile, i criteri utilizzati per
+determinare tale periodo; l’esistenza di un processo decisionale automatizzato; il diritto a proporre
+reclamo all’autorità di controllo; il diritto ad essere informato se la comunicazione dei dati
+costituisce obbligo legale o contrattuale o un requisito necessario per la conclusione del contratto.
+4. l’interessato ha altresì diritto di ottenere: a) l’aggiornamento, la rettificazione ovvero, quando vi
+ha interesse, l’integrazione dei dati; b) la cancellazione, la trasformazione in forma anonima o il
+blocco dei dati trattati in violazione di legge, compresi quelli di cui non è necessaria la
+conservazione in relazione agli scopi per i quali i dati sono stati raccolti o successivamente trattati;
+c) l’attestazione che le operazioni di cui alle lettere a) e b) del presente comma sono state portate a
+conoscenza, anche per quanto riguarda il loro contenuto; il nominativo di coloro ai quali i dati sono
+stati comunicati o diffusi, eccettuato il caso in cui tale adempimento si riveli impossibile o comporti
+un impiego di mezzi manifestamente sproporzionato rispetto al diritto tutelato.
+5. l’interessato ha infine diritto di opporsi, in tutto o in parte: a) per motivi legittimi al trattamento
+dei dati personali che lo riguardano, ancorché pertinenti lo scopo della raccolta; b) al trattamento di
+dati personali che lo riguardano a fini di invio di materiale pubblicitario o di vendita diretta o per il
+compimento di ricerche di mercato o di comunicazione commerciale.
+L'interessato può esercitare i diritti sopra elencati contattando il Responsabile della Protezione dei
+Dati al seguente indirizzo: MAIL DEL CLIENTE
+
+RAGIONE SOCIALE CLIENTE, provvederà senza ritardo a fornire idoneo riscontro, e
+comunque entro un termine non superiore a mesi 1. Nel caso di particolare complessità il termine
+per la risposta, a norma del Regolamento, può essere di mesi 3. In ogni caso il titolare informerà
+l’interessato entro mesi 1 dalla richiesta, anche per comunicarne il diniego nei casi specificatamente
+contemplati nelle disposizioni vigenti.
+
+9. DIRITTI A PORRE RECLAMO ALL'AUTORITÀ DI CONTROLLO
+Si informa altresì che ai sensi degli artt. 77 e ss del Reg. UE 2016/679, ove ritenesse lesi i propri
+diritti, l’interessato può rivolgersi al Garante per la protezione dei dati personali nonché all’autorità
+giudiziaria ordinaria.
+
+10. LINGUA
+Qualunque traduzione di questo documento in lingua diversa dall’italiana è fornita a solo scopo
+informativo. In caso di discrepanze, l’unica versione legalmente valida e vincolante è la versione
+italiana di questo documento.
+
+<b>RAGIONE SOCIALE CLIENTE</b>
+Indirizzo
+P.IVA XXXXXXXX
+R.E.A. TV - XXXX
